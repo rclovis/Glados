d943edc59d07a967bff9f68f57934215cea93276
LeFunk <theo.liennard@epitech.eu>
2024-01-03 14:22:07 +0100

    double done

--- src/Bytecode.hs
+++ src/Bytecode.hs
@@ -10,56 +10,69 @@ import Data.Word (Word8, Word16, Word32, Word64)
 
 import qualified Data.ByteString as B
 
-data IntTypes = Int8Val Int8 | Int16Val Int16 | Int32Val Int32 | Int64Val Int64 deriving (Show, Eq)
+data IntTypes
+  = Int8Val Int8
+  | Int16Val Int16
+  | Int32Val Int32
+  | Int64Val Int64
+  deriving (Show, Eq)
 
-data WordTypes = Word8Val Word8 | Word16Val Word16 | Word32Val Word32 | Word64Val Word64 deriving (Show, Eq)
+data WordTypes
+  = Word8Val Word8
+  | Word16Val Word16
+  | Word32Val Word32
+  | Word64Val Word64
+  deriving (Show, Eq)
 
-data FloatingPoint = FloatVal Float | DoubleVal Double deriving (Show, Eq)
+data FloatingPoint
+  = FloatVal Float
+  | DoubleVal Double
+  deriving (Show, Eq)
 
 data Bytecode
-    = Funk Word8 Word8 -- function with int size of arg and int size of body
-    | Iload Word8 Word8 -- load int from stack
-    | Fload Word8 Word8 -- load float from stack
-    | Uload Word8 Word8 -- load uint from stack
-    | Istore Word8 Word8 -- store int to stack
-    | Fstore Word8 Word8 -- store float to stacktraverse toBin xs
-    | Ustore Word8 Word8 -- store uint to stack
-    | Iconst Word8 IntTypes -- push int to stack
-    | Fconst Word8 Float -- push float to stack
-    | Uconst Word8 WordTypes -- push uint to stack
-    | Iadd -- add int or uint
-    | Fadd -- add float
-    | Isub -- subtract int or uint
-    | Fsub -- subtract float
-    | Imul -- multiply int or uint
-    | Fmul -- multiply float
-    | Idiv -- divide int or uint
-    | Fdiv -- divide float
-    | Imod -- modulo int or uintù
-    | Ieq -- equal int or uint
-    | Feq -- equal float
-    | Ineq -- not equal int or uint
-    | Fneq -- not equal float
-    | Igt -- greater than int or uint
-    | Fgt -- greater than float
-    | Ilt -- less than int or uint
-    | Flt -- less than float
-    | Ige -- greater than or equal int or uint
-    | Fge -- greater than or equal float
-    | Ile -- less than or equal int or uint
-    | Fle -- less than or equal float
-    | Iand -- and int or uint
-    | Ior -- or int or uint
-    | Ixor -- xor int or uint
-    | Ift Word8 Int -- if true jump of int bytes
-    | Iff Word8 Int -- if false jump of int bytes
-    | Goto Word8 Int -- jump of int bytes
-    | Invoke Word8 Int -- invoke function at int index
-    | Return -- return from function
-    | I2f -- convert int to float
-    | F2i -- convert float to int
-    | Consume Word8 Word8 -- consume Int args of the stack 
-    | NOTHING -- do nothing
+  = Funk Word8 Word8 -- function with int size of arg and int size of body
+  | Iload Word8 Word8 -- load int from stack
+  | Fload Word8 Word8 -- load float from stack
+  | Uload Word8 Word8 -- load uint from stack
+  | Istore Word8 Word8 -- store int to stack
+  | Fstore Word8 Word8 -- store float to stacktraverse toBin xs
+  | Ustore Word8 Word8 -- store uint to stack
+  | Iconst Word8 IntTypes -- push int to stack
+  | Fconst Word8 FloatingPoint -- push float to stack
+  | Uconst Word8 WordTypes -- push uint to stack
+  | Iadd -- add int or uint
+  | Fadd -- add float
+  | Isub -- subtract int or uint
+  | Fsub -- subtract float
+  | Imul -- multiply int or uint
+  | Fmul -- multiply float
+  | Idiv -- divide int or uint
+  | Fdiv -- divide float
+  | Imod -- modulo int or uintù
+  | Ieq -- equal int or uint
+  | Feq -- equal float
+  | Ineq -- not equal int or uint
+  | Fneq -- not equal float
+  | Igt -- greater than int or uint
+  | Fgt -- greater than float
+  | Ilt -- less than int or uint
+  | Flt -- less than float
+  | Ige -- greater than or equal int or uint
+  | Fge -- greater than or equal float
+  | Ile -- less than or equal int or uint
+  | Fle -- less than or equal float
+  | Iand -- and int or uint
+  | Ior -- or int or uint
+  | Ixor -- xor int or uint
+  | Ift Word8 Int -- if true jump of int bytes
+  | Iff Word8 Int -- if false jump of int bytes
+  | Goto Word8 Int -- jump of int bytes
+  | Invoke Word8 Int -- invoke function at int index
+  | Return -- return from function
+  | I2f -- convert int to float
+  | F2i -- convert float to int
+  | Consume Word8 Word8 -- consume Int args of the stack 
+  | NOTHING -- do nothing
 
 word8toChar :: [Word8] -> B.ByteString
 word8toChar = B.pack
@@ -112,8 +125,44 @@ floatingStandardtoWord8 str = toWord8 (take 8 str) : floatingStandardtoWord8 (dr
     toWord8 :: [Char] -> Word8
     toWord8 st = toEnum $ foldl (\acc x -> acc * 2 + digitToInt x) 0 st
 
-floatToW8 :: Float -> [Word8]
-floatToW8 f = floatingStandardtoWord8 $ getIEEE f
+floatingPointToW8 :: FloatingPoint -> [Word8]
+floatingPointToW8 (FloatVal f) = floatingStandardtoWord8 $ getIEEE f
+floatingPointToW8 (DoubleVal d) = floatingStandardtoWord8 $ getDIEEE d
+
+getDIEEE :: Double -> [Char]
+getDIEEE f = getSignD f ++ getExponentD t ++ getMantissaD m
+  where
+    t = toBinary $ abs (truncate f)
+    m = tail (doubleToBinary $ abs f)
+
+doubleToBinary :: Double -> [Char]
+doubleToBinary f =
+  let integerPart = abs (truncate f)
+      fractionalPart = abs f - fromIntegral integerPart
+      intBinary = toBinary integerPart
+      fracBinary = dfractionToBinary fractionalPart
+  in intBinary ++ fracBinary
+
+dfractionToBinary :: Double -> [Char]
+dfractionToBinary f = dfracToBinStr f []
+  where
+    dfracToBinStr _ acc | length acc >= 52 = acc -- Max 52-bit precision for demonstration
+    dfracToBinStr 0 acc = acc
+    dfracToBinStr x acc =
+      let dbl = x * 2
+          (i, f') = properFraction dbl :: (Int, Double)
+      in dfracToBinStr f' (acc ++ show i)
+
+getMantissaD :: [Char] -> [Char]
+getMantissaD f = f ++ padZeros (52 - length f) (toBinary 0)
+
+getExponentD :: [Char] -> [Char]
+getExponentD f = padZeros 11 (toBinary (length f - 1 + 1023))
+
+getSignD :: Double -> [Char]
+getSignD f
+  | f < 0 = "1"
+  | otherwise = "0"
 
 getIEEE :: Float -> [Char]
 getIEEE f = getSign f ++ getExponent t ++ getMantissa m
@@ -179,7 +228,7 @@ toBin (Istore a b) = [4, a, b]
 toBin (Fstore a b) = [5, a, b]
 toBin (Ustore a b) = [6, a, b]
 toBin (Iconst a b) = [7, a] ++ intTypesTo8bit b
-toBin (Fconst a b) = [8, a] ++ floatToW8 b
+toBin (Fconst a b) = [8, a] ++ floatingPointToW8 b
 toBin (Uconst a b) = [9, a] ++ wordTypesTo8bit b
 toBin Iadd = [10]
 toBin Fadd = [11]
@@ -238,28 +287,28 @@ toHumanReadable Fmul = "  Fmul\n"
 toHumanReadable Idiv = "  Idiv\n"
 toHumanReadable Fdiv = "  Fdiv\n"
 toHumanReadable Imod = "  Imod\n"
-toHumanReadable Ieq = " Ieq\n"
-toHumanReadable Feq = " Feq\n"
+toHumanReadable Ieq = "  Ieq\n"
+toHumanReadable Feq = "  Feq\n"
 toHumanReadable Ineq = "  Ineq\n"
 toHumanReadable Fneq = "  Fneq\n"
-toHumanReadable Igt = " Igt\n"
-toHumanReadable Fgt = " Fgt\n"
-toHumanReadable Ilt = " Ilt\n"
-toHumanReadable Flt = " Flt\n"
-toHumanReadable Ige = " Ige\n"
-toHumanReadable Fge = " Fge\n"
-toHumanReadable Ile = " Ile\n"
-toHumanReadable Fle = " Fle\n"
+toHumanReadable Igt = "  Igt\n"
+toHumanReadable Fgt = "  Fgt\n"
+toHumanReadable Ilt = "  Ilt\n"
+toHumanReadable Flt = "  Flt\n"
+toHumanReadable Ige = "  Ige\n"
+toHumanReadable Fge = "  Fge\n"
+toHumanReadable Ile = "  Ile\n"
+toHumanReadable Fle = "  Fle\n"
 toHumanReadable Iand = "  Iand\n"
-toHumanReadable Ior = " Ior\n"
+toHumanReadable Ior = "  Ior\n"
 toHumanReadable Ixor = "  Ixor\n"
-toHumanReadable (Ift _ b) = " Ift " ++ show b ++ "\n"
-toHumanReadable (Iff _ b) = " Iff " ++ show b ++ "\n"
+toHumanReadable (Ift _ b) = "  Ift " ++ show b ++ "\n"
+toHumanReadable (Iff _ b) = "  Iff " ++ show b ++ "\n"
 toHumanReadable (Goto _ b) = "  Goto " ++ show b ++ "\n"
 toHumanReadable (Invoke _ b) = "  Invoke " ++ show b ++ "\n"
 toHumanReadable Return = "  Return\n"
-toHumanReadable I2f = " I2f\n"
-toHumanReadable F2i = " F2i\n"
+toHumanReadable I2f = "  I2f\n"
+toHumanReadable F2i = "  F2i\n"
 toHumanReadable (Consume _ b) = "  Consume " ++ show b ++ "\n"
 toHumanReadable NOTHING = "\n"
 
@@ -276,5 +325,6 @@ bytecode =
       Iconst 4 (Int32Val 1),
       Iconst 4 (Int32Val 2),
       Invoke 1 0,
-      Uconst 2 (Word16Val 255)
+      Uconst 2 (Word16Val 255),
+      Fconst 8 (DoubleVal 154541.452)
     ]
